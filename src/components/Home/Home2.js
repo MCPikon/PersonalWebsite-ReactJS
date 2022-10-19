edd1d2bba576cd82ba3b8b5970118fe2474507c0
Javier Picón <82212139+MCPikon@users.noreply.github.com>
2022-10-19 20:04:56 +0200

    first commit

---
+++ src/components/Home/Home2.js
@@ -0,0 +1,108 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import myImg from "../../Assets/avatar.svg";
+import Tilt from "react-parallax-tilt";
+import {
+  AiFillGithub,
+  AiOutlineTwitter,
+  AiFillInstagram,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
+function Home2() {
+  return (
+    <Container fluid className="home-about-section" id="about">
+      <Container>
+        <Row>
+          <Col md={8} className="home-about-description">
+            <h1 style={{ fontSize: "2.6em" }}>
+              DEJA <span className="purple"> QUE ME </span> PRESENTE
+            </h1>
+            <p className="home-about-body">
+              Soy una persona muy curiosa y me gusta
+              investigar sobre tecnologías que no conozco y saber más
+              sobre las que ya domino
+              <br />
+              <br />Me manejo bastante bien con
+              <i>
+                <b className="purple"> Java, HTML, CSS y JavaScript. </b>
+              </i>
+              <br />
+              <br />
+              No dispongo de mucha experiencia, pero me sobran
+              ganas de aprender y de aplicar todos mis esfuerzos
+              en mi trabajo.
+              <br />
+              <br />
+              <i>
+                Ahora mismo estoy aprendiendo por mi cuenta
+                <b className="purple"> Python </b> y
+                librerías de JavaScript como{" "}
+                <b className="purple">
+                  React.js y demás...
+                </b>
+                
+              </i>
+            </p>
+          </Col>
+          <Col md={4} className="myAvtar">
+            <Tilt>
+              <img src={myImg} className="img-fluid" alt="avatar" />
+            </Tilt>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12} className="home-about-social">
+            <h1>ENCUENTRAME EN</h1>
+            <p>
+              No dudes en <span className="purple">contactar </span>conmigo
+            </p>
+            <ul className="home-about-social-links">
+              <li className="social-icons">
+                <a
+                  href="https://github.com/MCPikon"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiFillGithub />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://twitter.com/MCPikon"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiOutlineTwitter />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.linkedin.com/in/javierpiconpastor/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <FaLinkedinIn />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.instagram.com/mcpikon"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour home-social-icons"
+                >
+                  <AiFillInstagram />
+                </a>
+              </li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+export default Home2;
